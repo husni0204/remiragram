fe22609f16481b1f7382401a103bf82b03da7136
Husni Mubarok <husnirezky2@gmail.com>
2026-03-31 17:34:39 +0700

    implementasi login belum selesai menit 21:21

--- src/app/(auth)/index.tsx
+++ src/app/(auth)/index.tsx
@@ -1,35 +1,62 @@
+import customAPI from "@/src/config/api";
+import { useAuthStore } from "@/src/stores/authStore";
 import { Link } from "expo-router";
-import React from "react";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const { setTokenData } = useAuthStore<string>();
+
+  const handleLogin = async () => {
+    setLoading(true);
+    try {
+      const { data } = await customAPI.post("/auth/login", {
+        email: email,
+        password: password,
+      });
+
+      await setTokenData(data.data, data.token);
+      Alert.alert("Success", "Berhasil Login");
+    } catch (error: any) {
+      const errorMsg = error.response.data.message;
+      const errorData = Array.isArray(errorMsg) ? errorMsg[0] : errorMsg;
+      Alert.alert("Error", errorData);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <SafeAreaView className="flex-1 justify-center px-8 bg-background">
+    <SafeAreaView className="justify-center flex-1 px-8 bg-background">
       {/* Logo */}
       <View className="items-center">
         <Text className="text-4xl font-title text-active">RemiraGram</Text>
-        <Text className="text-gray-500 mt-2">Welcome 😀</Text>
+        <Text className="mt-2 text-gray-500">Welcome 😀</Text>
       </View>
       {/* Form */}
       <View className="mt-5">
         <TextInput
           placeholder="Email"
-          className="bg-gray-300 px-4 rounded-xl mb-4"
+          className="px-4 mb-4 bg-gray-300 rounded-xl"
         />
         <TextInput
           placeholder="Password"
-          className="bg-gray-300 px-4 rounded-xl mb-4"
+          className="px-4 mb-4 bg-gray-300 rounded-xl"
           secureTextEntry
         />
-        <TouchableOpacity className="bg-active p-4 rounded-xl items-center mb-6">
-          <Text className="text-white font-semibold text-base">Login</Text>
+        <TouchableOpacity className="items-center p-4 mb-6 bg-active rounded-xl">
+          <Text className="text-base font-semibold text-white">Login</Text>
         </TouchableOpacity>
       </View>
       {/* Footer */}
-      <View className="items-center mb-10 flex-row justify-center">
+      <View className="flex-row items-center justify-center mb-10">
         <Text className="text-gray-500">Don&apos;t have an account ? </Text>
-        <Link href={"/(auth)/register"} className="text-active font-semibold">
+        <Link href={"/(auth)/register"} className="font-semibold text-active">
           Sign up
         </Link>
       </View>
---
+++ src/config/api.ts
@@ -0,0 +1,19 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+
+const baseURL = process.env.EXPO_PUBLIC_API_URL;
+
+const customAPI = axios.create({
+  baseURL: baseURL,
+});
+
+customAPI.interceptors.request.use(async (config) => {
+  const auth = await AsyncStorage.getItem("auth");
+  if (auth) {
+    const { token } = JSON.parse(auth);
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default customAPI;
